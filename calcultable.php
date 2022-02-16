996a40883eeb0731732f12777cc34ca8274d5a85
yankou945 <75915452+yankou945@users.noreply.github.com>
2022-02-16 16:15:14 +0100

    Add files via upload

---
+++ calcultable.php
@@ -0,0 +1,10 @@
+<?php
+for ($i = 1; $i <= 10; $i++)
+{
+    Echo 'Table de multiplication de '.$i.'<br>';
+    for ($j = 1; $j <= 10; $j++)
+    {
+        echo $i.' x '.$j.' = '.$i*$j.'<br>';
+    }
+}
+?>
